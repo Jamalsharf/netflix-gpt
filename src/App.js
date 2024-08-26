c8a5426f67bad8fe46cf76a58ac5b368cd0aa3eb
Jamalsharf <selvajamaludeen952@gmail.com>
2024-08-26 21:04:14 +0530

    Login page & sign in form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-
-import './App.css';
+import Body from "./components/Body";
 
 function App() {
- return <div className='text-3xl font-bold text-green-600'>Hello World</div>
+  return <Body />;
 }
 
 export default App;
